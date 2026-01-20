9402ec66154f559d8e5ff2ad5d47e796425122c0
wtc <thmashecpt024@student.wethinkcode.co.za>
2026-01-20 12:31:17 +0200

    Refactor HTML structure and enhance JavaScript functionality for book display

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 const myLibrary = []
-
+const booksContainer = document.querySelector(".books")
 
 function Book() {
 //. Constructor for Book object
@@ -11,4 +11,20 @@ function addBookToLibrary(title, author, pages, read) {
   const book = new Book(title, author, pages, read);
   myLibrary.push(book);
   return book;
+}
+function displayBooks(){
+    myLibrary.forEach(book => {
+      
+        const bookElement = document.createElement("div");
+        const {title, author, pages, read} = book
+        bookElement.classList.add("book");
+        bookElement.innerHTML = `
+            <h3>${title}</h3>
+            <p>Author: ${author}</p>
+            <p>Pages: ${pages}</p>
+            <p>Read: ${read ? "Yes" : "No"}</p>
+        `;
+        booksContainer.appendChild(bookElement);
+       
+    })
 }
